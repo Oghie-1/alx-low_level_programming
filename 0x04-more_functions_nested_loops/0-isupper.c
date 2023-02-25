9abd465d6ebeb7e2064026b6283f22b041c9d238
AfroTyche <aigbemanuel@gmail.com>
2023-02-25 02:31:31 -0800

    refixed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,13 +6,9 @@
  */
 
 int _isupper(int c)
-{
-char alpha;
-for (alpha = 'A'; alpha <= 'Z'; alpha++)
-{
-	if (c == alpha)
-		return (1);
-}
-
-return (0);
-}
+ {
+ 	if ((c >= 'A') && (c <= 'Z'))
+ 		return (1);
+ 	else
+ 		return (0);
+ }
