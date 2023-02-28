2ecb4bc6b4c1a3b80ff6392640a22d080d1f180f
AfroTyche <aigbemanuel@gmail.com>
2023-02-28 04:41:48 -0800

    Intitial commit

---
+++ 0x05-C_Pointers_Arrays_Strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+int reset_to_98(int c);
+void reset_to_98(int *n);
+
+#endif
+
+~                                                             
+~
