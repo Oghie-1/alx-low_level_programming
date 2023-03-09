20ff1c18d0e45cd88b917edef92eecca7b14b786
AfroTyche <aigbemanuel@gmail.com>
2023-03-08 21:57:42 -0800

    added files 0-3

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints a function of string in reverse
+ * @s: a reverse string
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
