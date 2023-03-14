897c54a565d14bfeaefba0209bd32fc8199dfbcd
AfroTyche <aigbemanuel@gmail.com>
2023-03-13 18:41:29 -0700

    fixed space issue

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strlen - counts the length of a string
  * @s: input string
@@ -8,16 +7,13 @@
 int _strlen(char *s)
 {
 int len = 0;
-
 while (*s != '\0')
 {
 len++;
 s++;
 }
-
 return (len);
 }
-
 /**
  * _strcpy - copies a string
  * @dest: destination string
@@ -27,16 +23,13 @@ return (len);
 char *_strcpy(char *dest, char *src)
 {
 int i;
-
 for (i = 0; src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
 dest[i] = '\0';
-
 return (dest);
 }
-
 /**
  * _strdup - duplicates a string
  * @str: string to be duplicated
@@ -46,22 +39,16 @@ char *_strdup(char *str)
 {
 char *new_str;
 int len;
-
 if (str == NULL)
 {
 return (NULL);
 }
-
 len = _strlen(str);
-
 new_str = malloc(sizeof(char) * (len + 1));
-
 if (new_str == NULL)
 {
 return (NULL);
 }
-
 _strcpy(new_str, str);
-
 return (new_str);
 }
